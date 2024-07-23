dab00b6319c17984be6bb32b752ed09d9ef28f65
Luc Soret <33832780+lucsoret@users.noreply.github.com>
2024-07-23 23:02:33 +0200

    Hotfix/historical ingestion (#19)
    * cache
    
    * update

--- hubeau-client/hubeau-client/hubeau_client/historical.py
+++ hubeau-client/hubeau-client/hubeau_client/historical.py
@@ -8,12 +8,12 @@ from google.cloud import storage
 from airflow.operators.python import PythonOperator
 from airflow.decorators import task
 from airflow.providers.google.cloud.hooks.gcs import GCSHook
+from datetime import datetime
 
 CODE_ENTITE = "K*"
 client = storage.Client()
 temp_file_path = "tempFile"
 
-@task
 def extract(date_start = pendulum.now()):
     base_url = "https://hubeau.eaufrance.fr/api/v1/hydrometrie/obs_elab"
     params = {
@@ -33,39 +33,27 @@ def extract(date_start = pendulum.now()):
 
         params["size"] = size
         response = requests.get(base_url, params=params)
+        print(f"count [{size}]  | size [{len(response.json()['data'])}]")
         return response.json()
     else:
         response.raise_for_status()
 
-
-@task
 def load(data, date_start = pendulum.now()):
     if data:
-        gcs_hook = GCSHook(gcp_conn_id='google_cloud_default')
-
         bucket_name =  os.environ.get("GCP_BUCKET_NAME", "riverflood-lewagon-dev")
         gcs_path_root = 'hubeau_data_historical'
         year, month, day = date_start.split('-')
 
-        target_gcs_path = f"{gcs_path_root}/{year}/{month}/{day}/obs_elab_{date_start}.json"
+        target_gcs_path = f"{gcs_path_root}/{year}/obs_elab_{date_start}.json"
         json_data = json.dumps(data)
-        gcs_hook.upload(
-            bucket_name=bucket_name,
-            object_name=target_gcs_path,
-            data=json_data
-        )
 
-        print("Data succesfully Loaded")
+        bucket = client.get_bucket(bucket_name)
+        blob = bucket.blob(target_gcs_path)
+        blob.upload_from_string(json_data)
 
 
-# Define the DAG
-with DAG(
-    "historical_ingestion",
-    description="Historical ingestion of Hub'Eau Hydrometrie API",
-    schedule_interval= '@daily',
-    catchup=True,
-    start_date=pendulum.today("UTC").add(days=365),
-    default_args = {"depends_on_past": False}
-)  as dag:
-    extract_task = extract('{{ ds }}')
-    load_task = load(extract_task, '{{ ds }}')
+if __name__ == "__main__":
+    for tdelta in range(365):
+        dt = (datetime.today() - timedelta(days=tdelta)).strftime('%Y-%m-%d')
+        j = extract(dt)
+        load(j, date_start=dt)
