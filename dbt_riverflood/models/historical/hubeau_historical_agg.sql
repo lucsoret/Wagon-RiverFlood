e6f96fc110dd179328e4386cc6db43c625249247
Luc Soret <33832780+lucsoret@users.noreply.github.com>
2024-07-30 22:18:42 +0200

    update (#46)

--- dbt_riverflood/models/historical/hubeau_historical_agg.sql
+++ dbt_riverflood/models/historical/hubeau_historical_agg.sql
@@ -8,10 +8,13 @@ SELECT
   latitude,
   longitude,
   grandeur_hydro_elab,
-  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(990)]/10 AS quantile_99,
-  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(900)]/10 AS quantile_90,
-  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(100)]/10 AS quantile_10,
-  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(10)]/10 AS quantile_01,
+  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(999)]/10 AS quantile_999,
+  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(990)]/10 AS quantile_990,
+  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(900)]/10 AS quantile_900,
+  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(100)]/10 AS quantile_100,
+  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(10)]/10 AS quantile_010,
+  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(1)]/10 AS quantile_001,
+
   min(date_obs_elab) as minimum_date_window,
   max(date_obs_elab) as maximum_date_window
 FROM
