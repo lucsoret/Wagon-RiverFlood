5406712520268a94e7a9f7c56e53adacb6640b37
Luc Soret <luc.soret@gmail.com>
2024-08-01 14:06:27 +0000

    fixed a big mistake

--- dbt_riverflood/models/historical/hubeau_historical_agg.sql
+++ dbt_riverflood/models/historical/hubeau_historical_agg.sql
@@ -8,12 +8,12 @@ SELECT
   latitude,
   longitude,
   grandeur_hydro_elab,
-  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(999)]/10 AS quantile_999,
-  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(990)]/10 AS quantile_990,
-  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(900)]/10 AS quantile_900,
-  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(100)]/10 AS quantile_100,
-  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(10)]/10 AS quantile_010,
-  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(1)]/10 AS quantile_001,
+  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(999)] AS quantile_999,
+  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(990)] AS quantile_990,
+  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(900)] AS quantile_900,
+  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(100)] AS quantile_100,
+  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(10)] AS quantile_010,
+  APPROX_QUANTILES(resultat_obs_elab, 1000)[OFFSET(1)] AS quantile_001,
 
   min(date_obs_elab) as minimum_date_window,
   max(date_obs_elab) as maximum_date_window
